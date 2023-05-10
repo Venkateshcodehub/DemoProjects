ab118d009b9218b79a8f03d87d0d73a1e1a85840
VenkataM18 <venkata83.maddu@gmail.com>
2023-05-10 17:01:44 +0530

    Initial commit to github

--- src/main/java/com/product/service/controller/ProductController.java
+++ src/main/java/com/product/service/controller/ProductController.java
@@ -1,20 +1,15 @@
 package com.product.service.controller;
 
 import com.product.service.entity.Product;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
 
     @PostMapping("/getProductDetails")
-    public ResponseEntity<String> getProductDetails(@RequestBody Product product){
-        return new ResponseEntity<String>();
+    public String getProductDetails(@RequestBody Product product){
+        return "response";
     }
 }
--- src/main/java/com/product/service/entity/Product.java
+++ src/main/java/com/product/service/entity/Product.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@lombok
 @Setter
 @Getter
 @NoArgsConstructor
